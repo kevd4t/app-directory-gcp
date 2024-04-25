adbff2e4801720ce64f741bd86c833c974fc8001
kevd4t <k.blanco@d4t.tech>
2024-04-25 11:20:17 -0400

    update config

---
+++ dist/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1CT+DZvRTServCQqOk844rEfh5iLkyUgswS0B+DvTJY=\"\n}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  compiler: { removeConsole: false },
+  images: { unoptimized: true },
+  output: 'standalone',
+  distDir: 'dist'
+}
 
-export default nextConfig;
+export default nextConfig
